562f4369f86f44b2b835930cbc262b9eeb69faad
jackgle <jglebien@gmail.com>
2017-07-11 15:20:00 -0500

    comment exchk.r

--- exchk.r
+++ exchk.r
@@ -10,16 +10,21 @@ library(data.table)
 args = commandArgs(trailingOnly=TRUE)
 cat("\nInput gff3:",args[1],"\n")
 cat("\nGenerating exon annotation... \n")
+# Parse gff3 to create exon data file
+# 3 columns: chromosome name, exon start, exon end
 system(paste("./filtgff3.bash",args[1]))
 cat("Done.\n")
-
+# Read exon data file
 genann = read.table(paste(args[1],".filt", sep=""))
+# Only consider locations in the provided chromosome
 chrmann = genann[genann$V1 == args[2],]
 if (nrow(chrmann)==0){
 	stop("Flawed chromosome name, 1-22, X, Y, or MT")
 }
 
 bkpt <- as.numeric(args[3])
+# Function for checking if breakpoint lies in an exon
+# Returns 1 if true, 0 if false
 excheck <- function(ann, bpt) {
 	tmp = between(bpt,ann[,2],ann[,3])
 	length(which(tmp==TRUE))
