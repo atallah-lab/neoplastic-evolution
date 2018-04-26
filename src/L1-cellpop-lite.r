18f35f9cfe4f72962ffc03764d16f39bad6378fd
jackgle <jglebien@gmail.com>
2018-04-26 00:54:48 -0500

    update L1-cellpop*

--- src/L1-cellpop-lite.r
+++ src/L1-cellpop-lite.r
@@ -2,13 +2,13 @@
 
 # Arguments:
 #
-# (1) - mode
+# (1) - run type ('single', 'batch', 'endless', see below)
 # (2) - number of timesteps
 # (3) - output file name
 # 
-# 'single' mode runs the simulation once for the specified number of timesteps and saves the result
-# 'batch' mode runs the simulation a number of times (see batch section) while varying parameters, and saves results separately
-# 'endless' mode runs the simulation endlessly (until manual termination) and saves the results to the same file after each timestep 
+# 'single' runs the simulation once for the specified number of timesteps and saves the result
+# 'batch' runs the simulation a number of times (see batch section) while varying parameters, and saves results separately
+# 'endless' runs the simulation endlessly (until manual termination) and saves the results to the same file after each timestep 
 
 #--- Load libraries and necessary data files, and define global variables
 ######################################################################################
@@ -18,16 +18,15 @@ library(Biostrings)
 library(BSgenome.Hsapiens.UCSC.hg38)
 library(GenomicRanges)
 genome <- Hsapiens
-source("process_L1s.r")
-source("mapsequence.r")
+load("../data/exann.rda")
 load("../data/chrmpd.rda") # load chromosome probability distribution
 load("../data/L1RankTable.rda")
-load("../data/exann.rda")
+load('../data/chromSitePd.rda')
 trpd <- read.table("../data/L1truncpd.csv",sep=",")
 tdpd <- read.table("../data/L1transdpd.csv",sep=",")
 for (i in names(Hsapiens)[1:24]){ # load all chromosome map files
-	cat(paste0("Loading map file...",i,"\n"))
-    load(paste0("../data/root_maps/",i,".rda"))
+        cat(paste0("Loading map file... ",i,"\n"))
+        load(paste0("../data/root_maps/",i,".rda"))
 }
 strdict<-c("+","-")
 names(strdict)<-c(1,2)
@@ -40,12 +39,11 @@ args<-commandArgs(trailingOnly=TRUE)
 # PURPOSE: To simulate the transposition of an L1 sequence in a given genome
 #
 # INPUT:
-#   genome        (BSgenome) reference genome
-#   node          (data.tree node) input node of tree
 #   copyNum       (integer) number of L1 insertions to simulate
 #
 # OUTPUT: (list) inserted sequences, sites, and strand
-gen_sim <- function(genome,node,copyNum) {
+
+gen_sim <- function(copyNum) {
 
         sites_loci<-c() # Initialize arrays for storing simulated ins. site data
         sites_chrm<-c()
@@ -60,9 +58,6 @@ gen_sim <- function(genome,node,copyNum) {
 
         for (chrnm in names(chrmlist)) {
 
-
-#                 cat("\nChromosome: ",chrnm)
-
                 map<-get(paste0(chrnm,"Map"))
                 ict<-map[[2]]
                 icl<-map[[3]]
@@ -70,19 +65,10 @@ gen_sim <- function(genome,node,copyNum) {
                 iol<-map[[5]]
                 insites<-map[[1]]
 
-
                 chrcopyNum<-chrmlist[[chrnm]]
 
-                pd <- c(11.55*length(which(!is.na(ict))),
-                        7.25*length(which(!is.na(icl))),
-                        1.95*length(which(!is.na(iot))),
-                        1*length(which(!is.na(iol))))
-                pd <- (pd/sum(pd))*(1-ENifrc)
-                pd <- append(pd,ENifrc)
-#                 cat("\nSite class distribution:\n",pd)
-
                 #--- Generates insertion sites
-                classes <- sample(x = c(1:5),chrcopyNum,replace=TRUE,prob=pd)
+                classes <- sample(x = c(1:5),chrcopyNum,replace=TRUE,prob=pds[[chrnm]])
                 sites <- rep(0,chrcopyNum)
                 strand <-rep(0,chrcopyNum)
 
@@ -117,13 +103,15 @@ gen_sim <- function(genome,node,copyNum) {
 
         }
 
-        #--- Creates sequences for insertion
-        tmp <- process_L1s(genome,L1RankTable,trpd,tdpd,copyNum)
-        l1s <- tmp[[1]]
-        l1indcs <- tmp[[2]]
-        tdlen <- tmp[[3]]
-        trlen <- tmp[[4]]
-        return(list(l1s,sites_chrm,sites_loci,sites_strand,l1indcs))
+        # Sample copyNum L1s from the list (with replacement), based on activity ranking
+        l1indcs <- sample(x=c(1:40), copyNum, replace=TRUE, prob=L1RankTable$score[1:40])
+
+        # Sample copyNum truncation fractions and transduction lengths from their respective 
+        # probability densities trpd, and tdpd
+#         trfrc <- sample(x=trpd[[1]], copyNum, replace=TRUE, prob=trpd[[2]])
+#         tdlen <- sample(x=tdpd[[1]], copyNum, replace=TRUE, prob=tdpd[[2]])
+
+    return(list(sites_chrm,sites_loci,sites_strand,l1indcs))
 }
 
 
@@ -131,66 +119,78 @@ gen_sim <- function(genome,node,copyNum) {
 #
 # INPUT:
 #   r             (float) division rate
-#   anno       (data frame) Annotation of genes (i.e. chromosome   start   end)
+#   anno          (data frame) Annotation of genes (i.e. chromosome   start   end)
 #   sites_chrm    (numeric vector) chromosomes containing L1 insertions for the clone
 #   sites_loci    (character vector) insertion positions (in the respective chromosome)
+#   gainp         (factor by which the division rate changes if a non-TSG is disrupted)
+#   lossp         (factor by which the division rate changes if a TSG is disrupted)
 #
 # OUTPUT: (float) possibly updated division rate
 rank_clone <- function(r, anno, sites_chrm, sites_loci, gainp, lossp) {
 
     gene_hits=0; # set counter to zero
     tsg_hits=0;
+    genes=c();
+    
     for (i in 1:length(unique(sites_chrm))) { # loop over chromosomes inserted into
+        
         tmp=anno[anno$chrom==unique(sites_chrm)[i],] # reduce annotation table to entries for current chrom
-        chrmann_ntsg=tmp[tmp$istsg==0,]
-        chrmann_tsg =tmp[tmp$istsg==1,]
-        tmp = sites_loci[sites_chrm==unique(sites_chrm)[i]] # reduce insertion loci to entries for current chrom
-        tmp_hits = between(tmp,chrmann_ntsg$start,chrmann_ntsg$end) # create logical for insertions, whether into non-tsg-gene or not
-        gene_hits=gene_hits+length(which(tmp_hits==TRUE)) # count the number of non-tsg-gene insertions
-#         print(gene_hits)
-        tmp_hits  = between(tmp,chrmann_tsg$start,chrmann_tsg$end) # same for tsg-gene insertions
-        tsg_hits =tsg_hits+length(which(tmp_hits==TRUE))
+
+        tmp2 = sites_loci[sites_chrm==unique(sites_chrm)[i]] # reduce insertion loci to entries for current chrom
+        
+        ins <- lapply(tmp2,between,tmp$start,tmp$end) # create logical for insertions, whether into non-tsg or not
+        ins <- unlist(lapply(ins,which))
+        
+        if (length(ins)>0) {
+            genes <- append(genes,tmp$geneSym[ins])
+        }
+        
+        gene_hits=gene_hits+length(which(tmp$istsg[ins]==0)) # count the number of non-tsg insertions
+        tsg_hits =tsg_hits +length(which(tmp$istsg[ins]==1)) # count the number of tsg insertions
+
     }
 
     if (gene_hits > 0 || tsg_hits > 0){
-	    r=r*(lossp^gene_hits)*(gainp^tsg_hits)
+        r=r*(lossp^gene_hits)*(gainp^tsg_hits)
     }
 
-    return(r)
+    return(list(r,genes))
 }
+
 
 # PURPOSE: To call gen_sim.r with some probability (probability of transposition, tp) for a clone at a time step
 #
 # INPUT:
-#   node          (data.tree node) current node of the data tree
-#   tnum          (integer) time step number
+#   node        (data.tree node) current node of the data tree
+#   sp          passenger mutation selection coefficient (<1)
+#   sd          driver mutation selection coefficient (>1)
 #
 # OUTPUT: void
 maybeTranspose <- function(node, sd, sp) {
-    if (node$r==0){ # if the division rate of the clone is zero, skip the node
+
+    if (node$r[[1]]==0){ # if the division rate of the clone is zero, skip the node
         return()
     }
     
     # increase the number of cells by the existing number * the division rate factor
-    nc <- node$ncells[length(node$ncells)] + round(node$ncells[length(node$ncells)]*node$r)
+    nc <- node$ncells[length(node$ncells)] + round(node$ncells[length(node$ncells)]*node$r[[1]])
     
     # sample from binomial distribution for number of transpositions
-    if (nc < 4.2e9) {ntrans <- rbinom(1,nc,node$cellP)} # rbinom() fails for large n
-    else {ntrans <- nc*node$cellP} # If n is too large, use the expected number of events (mean of distribution)
+    if (nc < 4.2e9) {ntrans <- rbinom(1,nc,cellP)} # rbinom() fails for large n
+    else {ntrans <- nc*cellP} # If n is too large, use the expected number of events (mean of distribution)
     if (ntrans > 0) {
-        simout <- gen_sim(genome,node,ntrans)
+        simout <- gen_sim(ntrans)
         nc <- nc-ntrans
         for (i in 1:ntrans) {
             l<<-l+1
-            if (width(simout[[1]][i])>=6000) {clp_tmp <- min(node$cellP+rootCellP*L1RankTable$score[simout[[5]][i]], 1)} # Transposition P increases with intact L1 insertions   
-            else {clp_tmp <- node$cellP}
-            r_tmp <- rank_clone(node$r, exann, lapply(simout,'[',i)[[2]], lapply(simout,'[',i)[[3]], sd, sp)
+            r_tmp <- rank_clone(node$r, exann, lapply(simout,'[',i)[[1]], lapply(simout,'[',i)[[2]], sd, sp)
             tmp<-mapply(append, lapply(simout,'[',i), node$tes, SIMPLIFY = FALSE)
-            node$AddChild(l, cellP=clp_tmp, ncells=1, r=r_tmp, tes=tmp)
+            node$AddChild(l, ncells=1, r=r_tmp[[1]], genes=append(node$genes,r_tmp[[2]]), tes=tmp)
         }
     }   
     node$ncells <- append(node$ncells,nc)
 }
+
 
 
 #--- Set simulation parameters
@@ -199,7 +199,7 @@ maybeTranspose <- function(node, sd, sp) {
 ENifrc<- .1       # Fraction of endonuclease-independent (random) insertions
 rootNCells <- 1   # Initial number of cells in root clone
 rootDivRate <- 1  # Initial division rate
-rootCellP <- 0.2  # Probability of transposition / timestep of a single cell
+cellP <- 0.2  # Probability of transposition / timestep of a single cell
 
 NT <- args[2]     # Number of time steps
 
@@ -207,18 +207,18 @@ NT <- args[2]     # Number of time steps
 ######################################################################################
 
 if (args[1]=='batch') {
-	sd <- c(1/seq(0.8,0.2,-0.2),7)
-	sp <- seq(0.8,0.0,-0.2)
+    sd <- c(1/seq(0.8,0.4,-0.1))
+    sp <- seq(0.8,0.4,-0.1)
 	nrun <- 0
 	for (sdi in 1:5) {
 		for (spi in 1:5) {
-		    l<-1
+		    l<-1 # Clone counter
 		    CellPop <- Node$new(1)
 		    CellPop$ncells <- c(rootNCells)
 		    CellPop$r <- rootDivRate
-		    CellPop$tes <- list(DNAStringSet(),c(),c(),c(),c())
-            CellPop$cellP <- rootCellP
-            # CellPop$tes <- list(DNAStringSet(c("TCGA")),c("chr1"),c(1013467),c("+"))
+		    CellPop$tes <- list(c(),c(),c(),c())
+            CellPop$genes <- c()
+		    # CellPop$tes <- list(DNAStringSet(c("TCGA")),c("chr1"),c(1013467),c("+"),c(0))
 		    # CellPop$r <- rank_clone(CellPop$r, exann, CellPop$tes[[2]], CellPop$tes[[3]], 1.2, 0.8)
 		    # CellPop$r
 
@@ -238,13 +238,15 @@ if (args[1]=='batch') {
 
 	}
 } else if (args[1] == 'single') {
-    l<-1
+
+    l<-1 # Clone counter
 	CellPop <- Node$new(1)
 	CellPop$ncells <- c(rootNCells)
 	CellPop$r <- rootDivRate
-	CellPop$tes <- list(DNAStringSet(),c(),c(),c(),c())
-    CellPop$cellP <- rootCellP
-	# CellPop$tes <- list(DNAStringSet(c("TTATTTA")),c("chr1"),c(1001140),c("+"))
+	CellPop$tes <- list(c(),c(),c(),c())
+    CellPop$genes <- c()
+
+	# CellPop$tes <- list(DNAStringSet(c("TTATTTA")),c("chr1"),c(1001140),c("+"),c(0))
 	# CellPop$r <- rank_clone(CellPop$r, exann, CellPop$tes[[2]], CellPop$tes[[3]])
 	# CellPop$r
 
@@ -259,16 +261,18 @@ if (args[1]=='batch') {
 	save(CellPop, file=paste0(args[3]))
 
 } else if (args[1] == 'endless') {
-    l<-1
+    
+    l<-1 # Clone counter
 	CellPop <- Node$new(1)
 	CellPop$ncells <- c(rootNCells)
 	CellPop$r <- rootDivRate
-    CellPop$tes <- list(DNAStringSet(),c(),c(),c(),c())
-    CellPop$cellP <- rootCellP
+    CellPop$tes <- list(c(),c(),c(),c())
+    CellPop$genes <- c()
+
 	while (1) {
 		ptm <- proc.time()
 
-	    	CellPop$Do(maybeTranspose,1.25,0.8)
+	    CellPop$Do(maybeTranspose,1.25,0.8)
 		save(CellPop, file=paste0(args[3]))              
 
 		print(proc.time()-ptm)
--- src/L1-cellpop.r
+++ src/L1-cellpop.r
@@ -2,13 +2,13 @@
 
 # Arguments:
 #
-# (1) - mode
+# (1) - run type ('single', 'batch', 'endless', see below)
 # (2) - number of timesteps
 # (3) - output file name
 # 
-# 'single' mode runs the simulation once for the specified number of timesteps and saves the result
-# 'batch' mode runs the simulation a number of times (see batch section) while varying parameters, and saves results separately
-# 'endless' mode runs the simulation endlessly (until manual termination) and saves the results to the same file after each timestep 
+# 'single' runs the simulation once for the specified number of timesteps and saves the result
+# 'batch' runs the simulation a number of times (see batch section) while varying parameters, and saves results separately
+# 'endless' runs the simulation endlessly (until manual termination) and saves the results to the same file after each timestep 
 
 #--- Load libraries and necessary data files, and define global variables
 ######################################################################################
@@ -23,11 +23,12 @@ source("mapsequence.r")
 load("../data/chrmpd.rda") # load chromosome probability distribution
 load("../data/L1RankTable.rda")
 load("../data/exann.rda")
+# load('../data/exann.rda')
 trpd <- read.table("../data/L1truncpd.csv",sep=",")
 tdpd <- read.table("../data/L1transdpd.csv",sep=",")
 for (i in names(Hsapiens)[1:24]){ # load all chromosome map files
-	cat(paste0("Loading map file...",i,"\n"))
-    load(paste0("../data/root_maps/",i,".rda"))
+        cat(paste0("Loading map file... ",i,"\n"))
+        load(paste0("../data/root_maps/",i,".rda"))
 }
 strdict<-c("+","-")
 names(strdict)<-c(1,2)
@@ -45,6 +46,7 @@ args<-commandArgs(trailingOnly=TRUE)
 #   copyNum       (integer) number of L1 insertions to simulate
 #
 # OUTPUT: (list) inserted sequences, sites, and strand
+
 gen_sim <- function(genome,node,copyNum) {
 
         sites_loci<-c() # Initialize arrays for storing simulated ins. site data
@@ -132,33 +134,44 @@ gen_sim <- function(genome,node,copyNum) {
 #
 # INPUT:
 #   r             (float) division rate
-#   anno       (data frame) Annotation of genes (i.e. chromosome   start   end)
+#   anno          (data frame) Annotation of genes (i.e. chromosome   start   end)
 #   sites_chrm    (numeric vector) chromosomes containing L1 insertions for the clone
 #   sites_loci    (character vector) insertion positions (in the respective chromosome)
+#   gainp         (factor by which the division rate changes if a non-TSG is disrupted)
+#   lossp         (factor by which the division rate changes if a TSG is disrupted)
 #
 # OUTPUT: (float) possibly updated division rate
 rank_clone <- function(r, anno, sites_chrm, sites_loci, gainp, lossp) {
 
     gene_hits=0; # set counter to zero
     tsg_hits=0;
+    genes=c();
+    
     for (i in 1:length(unique(sites_chrm))) { # loop over chromosomes inserted into
+        
         tmp=anno[anno$chrom==unique(sites_chrm)[i],] # reduce annotation table to entries for current chrom
-        chrmann_ntsg=tmp[tmp$istsg==0,]
-        chrmann_tsg =tmp[tmp$istsg==1,]
-        tmp = sites_loci[sites_chrm==unique(sites_chrm)[i]] # reduce insertion loci to entries for current chrom
-        tmp_hits = between(tmp,chrmann_ntsg$start,chrmann_ntsg$end) # create logical for insertions, whether into non-tsg-gene or not
-        gene_hits=gene_hits+length(which(tmp_hits==TRUE)) # count the number of non-tsg-gene insertions
-#         print(gene_hits)
-        tmp_hits  = between(tmp,chrmann_tsg$start,chrmann_tsg$end) # same for tsg-gene insertions
-        tsg_hits =tsg_hits+length(which(tmp_hits==TRUE))
+
+        tmp2 = sites_loci[sites_chrm==unique(sites_chrm)[i]] # reduce insertion loci to entries for current chrom
+        
+        ins <- lapply(tmp2,between,tmp$start,tmp$end) # create logical for insertions, whether into non-tsg or not
+        ins <- unlist(lapply(ins,which))
+        
+        if (length(ins)>0) {
+            genes <- append(genes,tmp$geneSym[ins])
+        }
+        
+        gene_hits=gene_hits+length(which(tmp$istsg[ins]==0)) # count the number of non-tsg insertions
+        tsg_hits =tsg_hits +length(which(tmp$istsg[ins]==1)) # count the number of tsg insertions
+
     }
 
     if (gene_hits > 0 || tsg_hits > 0){
-	    r=r*(lossp^gene_hits)*(gainp^tsg_hits)
+        r=r*(lossp^gene_hits)*(gainp^tsg_hits)
     }
 
-    return(r)
+    return(list(r,genes))
 }
+
 
 # PURPOSE: Updates the gene annotation of the clone
 #
@@ -168,6 +181,7 @@ rank_clone <- function(r, anno, sites_chrm, sites_loci, gainp, lossp) {
 #   tes             (list of lists) Node tes
 #
 # OUTPUT: anno
+
 update_anno <- function(anno, simout, tes) {
     
     tmp = mapply(append, simout, tes, SIMPLIFY = FALSE)
@@ -193,6 +207,7 @@ update_anno <- function(anno, simout, tes) {
 #   l1s           (DNAStringSet) l1 sequences and orientation 
 #
 # OUTPUT: (list) updated chromosome annotation
+
 update_chrom_map <- function(chrnm,chrMap,sites_chrm,sites_loci,l1s) {
 
         if (length(which(sites_chrm==chrnm))==0){
@@ -238,26 +253,25 @@ maybeTranspose <- function(node, sd, sp) {
     }
     
     # increase the number of cells by the existing number * the division rate factor
-    nc <- node$ncells[length(node$ncells)] + ceiling(node$ncells[length(node$ncells)]*node$r)
+    nc <- node$ncells[length(node$ncells)] + round(node$ncells[length(node$ncells)]*node$r)
     
     # sample from binomial distribution for number of transpositions
-    if (nc < 4.2e9) {ntrans <- rbinom(1,nc,node$cellP)} # rbinom() fails for large n
-    else {ntrans <- nc*node$cellP} # If n is too large, use the expected number of events (mean of distribution)
+    if (nc < 4.2e9) {ntrans <- rbinom(1,nc,cellP)} # rbinom() fails for large n
+    else {ntrans <- nc*cellP} # If n is too large, use the expected number of events (mean of distribution)
     if (ntrans > 0) {
         simout <- gen_sim(genome,node,ntrans)
         nc <- nc-ntrans
         for (i in 1:ntrans) {
             l<<-l+1
-            if (width(simout[[1]][i])>=6000) {clp_tmp <- min(node$cellP+rootCellP*L1RankTable$score[simout[[5]][i]], 1)} # Transposition P increases with intact L1 insertions    
-            else {clp_tmp <- node$cellP}
             tmp <- update_anno(exann,lapply(simout,'[',i),node$tes)
             r_tmp <- rank_clone(node$r, tmp, lapply(simout,'[',i)[[2]], lapply(simout,'[',i)[[3]], sd, sp)
             tmp<-mapply(append, lapply(simout,'[',i), node$tes, SIMPLIFY = FALSE)
-            node$AddChild(l, cellP=clp_tmp, ncells=1, r=r_tmp, tes=tmp)
+            node$AddChild(l, ncells=1, r=r_tmp[[1]], genes=append(node$genes,r_tmp[[2]]), tes=tmp)
         }
     }   
     node$ncells <- append(node$ncells,nc)
 }
+
 
 
 #--- Set simulation parameters
@@ -266,7 +280,7 @@ maybeTranspose <- function(node, sd, sp) {
 ENifrc<- .1       # Fraction of endonuclease-independent (random) insertions
 rootNCells <- 1   # Initial number of cells in root clone
 rootDivRate <- 1  # Initial division rate
-rootCellP <- 0.2  # Probability of transposition / timestep of a single cell
+cellP <- 0.2  # Probability of transposition / timestep of a single cell
 
 NT <- args[2]     # Number of time steps
 
@@ -274,8 +288,8 @@ NT <- args[2]     # Number of time steps
 ######################################################################################
 
 if (args[1]=='batch') {
-	sd <- c(1/seq(0.8,0.2,-0.2),7)
-	sp <- seq(0.8,0.0,-0.2)
+    sd <- c(1/seq(0.8,0.4,-0.1))
+    sp <- seq(0.8,0.4,-0.1)
 	nrun <- 0
 	for (sdi in 1:5) {
 		for (spi in 1:5) {
@@ -283,8 +297,8 @@ if (args[1]=='batch') {
 		    CellPop <- Node$new(1)
 		    CellPop$ncells <- c(rootNCells)
 		    CellPop$r <- rootDivRate
-		    CellPop$tes <- list(DNAStringSet(),c(),c(),c(),c())
-            CellPop$cellP <- rootCellP
+            CellPop$tes <- list(DNAStringSet(c()),c(),c(),c(),c())
+            CellPop$genes <- c()
 		    # CellPop$tes <- list(DNAStringSet(c("TCGA")),c("chr1"),c(1013467),c("+"),c(0))
 		    # CellPop$r <- rank_clone(CellPop$r, exann, CellPop$tes[[2]], CellPop$tes[[3]], 1.2, 0.8)
 		    # CellPop$r
@@ -310,8 +324,9 @@ if (args[1]=='batch') {
 	CellPop <- Node$new(1)
 	CellPop$ncells <- c(rootNCells)
 	CellPop$r <- rootDivRate
-	CellPop$tes <- list(DNAStringSet(),c(),c(),c(),c())
-    CellPop$cellP <- rootCellP
+    CellPop$tes <- list(DNAStringSet(c()),c(),c(),c(),c())
+    CellPop$genes <- c()
+
 	# CellPop$tes <- list(DNAStringSet(c("TTATTTA")),c("chr1"),c(1001140),c("+"),c(0))
 	# CellPop$r <- rank_clone(CellPop$r, exann, CellPop$tes[[2]], CellPop$tes[[3]])
 	# CellPop$r
@@ -327,12 +342,13 @@ if (args[1]=='batch') {
 	save(CellPop, file=paste0(args[3]))
 
 } else if (args[1] == 'endless') {
+    
     l<-1 # Clone counter
 	CellPop <- Node$new(1)
 	CellPop$ncells <- c(rootNCells)
 	CellPop$r <- rootDivRate
-    CellPop$tes <- list(DNAStringSet(),c(),c(),c(),c())
-    CellPop$cellP <- rootCellP
+    CellPop$tes <- list(DNAStringSet(c()),c(),c(),c(),c())
+    CellPop$genes <- c()
 
 	while (1) {
 		ptm <- proc.time()
