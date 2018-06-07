8f68a71faeec3a4286b48c024714e545da69632b
manisha panta <5manisa.panta@gmail.com>
2018-06-07 14:27:28 -0500

    update L1-cellpop-lite-v2.ipynb

--- src/launch_sims.bash
+++ src/launch_sims.bash
@@ -1,22 +1,22 @@
 #!/bin/bash
 
-mkdir ../../Data/simout053018_9
-mkdir ../../Data/simout053018_10
-mkdir ../../Data/simout053018_11
-mkdir ../../Data/simout053018_12
+mkdir ../../Data/simout070718_9
+mkdir ../../Data/simout070718_10
+mkdir ../../Data/simout070718_11
+mkdir ../../Data/simout070718_12
 
-./L1-cellpop-lite.r batch 17 ../../Data/simout053018_9/out &
-echo $! > ../../Data/simout053018_9/pid.txt
-echo "./L1-cellpop-lite.r batch 17 ../../Data/simout053018_9 &" >> ../../Data/simout053018_9/pid.txt
+./L1-cellpop-lite.r batch 17 ../../Data/simout070718_9/out &
+echo $! > ../../Data/simout070718_9/pid.txt
+echo "./L1-cellpop-lite.r batch 17 ../../Data/simout070718_9 &" >> ../../Data/simout070718_9/pid.txt
 
-./L1-cellpop-lite.r batch 17 ../../Data/simout053018_10/out &
-echo $! > ../../Data/simout053018_10/pid.txt
-echo "./L1-cellpop-lite.r batch 17 ../../Data/simout053018_10 &" >> ../../Data/simout053018_10/pid.txt
+./L1-cellpop-lite.r batch 17 ../../Data/simout070718_10/out &
+echo $! > ../../Data/simout070718_10/pid.txt
+echo "./L1-cellpop-lite.r batch 17 ../../Data/simout070718_10 &" >> ../../Data/simout070718_10/pid.txt
 
-./L1-cellpop-lite.r batch 17 ../../Data/simout053018_11/out &
-echo $! > ../../Data/simout053018_11/pid.txt
-echo "./L1-cellpop-lite.r batch 17 ../../Data/simout053018_11 &" >> ../../Data/simout053018_11/pid.txt
+./L1-cellpop-lite.r batch 17 ../../Data/simout070718_11/out &
+echo $! > ../../Data/simout070718_11/pid.txt
+echo "./L1-cellpop-lite.r batch 17 ../../Data/simout070718_11 &" >> ../../Data/simout070718_11/pid.txt
 
-./L1-cellpop-lite.r batch 17 ../../Data/simout053018_12/out &
-echo $! > ../../Data/simout053018_12/pid.txt
-echo "./L1-cellpop-lite.r batch 17 ../../Data/simout053018_12 &" >> ../../Data/simout053018_12/pid.txt
+./L1-cellpop-lite.r batch 17 ../../Data/simout070718_12/out &
+echo $! > ../../Data/simout070718_12/pid.txt
+echo "./L1-cellpop-lite.r batch 17 ../../Data/simout070718_12 &" >> ../../Data/simout070718_12/pid.txt
