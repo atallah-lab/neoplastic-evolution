8eee9a4da4d675e7940d22edee9abe6e15f8314e
jackgle <jglebien@gmail.com>
2018-08-14 11:58:45 -0500

    update exon annotation, add cancer-specific mutation type pd's

--- src/launch_sims.bash
+++ src/launch_sims.bash
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-./L1-cellpop-lite-v22.r batch ../../sims062718/run 50 0 0 0 > ../../sims062718/run &
-echo $! > ../../sims062718/pid.txt
-./L1-cellpop-lite-v22.r batch ../../sims062718/run 100 0 0 0 > ../../sims062718/run &
-echo $! >> ../../sims062718/pid.txt
-#./L1-cellpop-lite-v22.r batch ../../sims062718/run 500 0 0 0 > ../../sims062718/run &
-#echo $! >> ../../sims062718/pid.txt
-#./L1-cellpop-lite-v22.r batch ../../sims062718/run 1000 0 0 0 > ../../sims062718/run &
-#echo $! >> ../../sims062718/pid.txt
-#./L1-cellpop-lite-v22.r batch ../../sims062718/run 2000 0 0 0 > ../../sims062718/run &
-#echo $! >> ../../sims062718/pid.txt
+./L1-cellpop-lite-v22.r batch ../../sims080818/run 50 0 0.1 0.001 &
+echo $! > ../../sims080818/pid.txt
+./L1-cellpop-lite-v22.r batch ../../sims080818/run 100 0 0.1 0.001 &
+echo $! >> ../../sims080818/pid.txt
+./L1-cellpop-lite-v22.r batch ../../sims080818/run 500 0 0.1 0.001 &
+echo $! >> ../../sims080818/pid.txt
+./L1-cellpop-lite-v22.r batch ../../sims080818/run 1000 0 0.1 0.001 &
+echo $! >> ../../sims080818/pid.txt
+./L1-cellpop-lite-v22.r batch ../../sims080818/run 2000 0 0.1 0.001 &
+echo $! >> ../../sims080818/pid.txt
